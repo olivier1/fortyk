d31aff38b79faedb4ada293ae8bbcd647c8f7df8
olivier1 <mancor161@gmail.com>
2020-05-29 14:36:03 -0400

    skills1
    made the basic grid framework for the skills, need to code in logic for common lores and navigate etc. also need to add an add button and a delete button.

--- module/handleBarsTemplates.js
+++ module/handleBarsTemplates.js
@@ -4,10 +4,10 @@ export const preloadHandlebarsTemplates = async function() {
   const templatePaths = [
 
     // Actor Sheet Partials
-   "systems/fortyk/templates/actors/actor-skills.html",
+   "systems/fortyk/templates/actor/actor-skills.html",
     "systems/fortyk/templates/actor/actor-tnt.html",
     "systems/fortyk/templates/actor/actor-exp.html",
-    "systems/fortyk/templates/actor/actor-weapons.html",
+    "systems/fortyk/templates/actor/actor-combat.html",
       "systems/fortyk/templates/actor/actor-gear.html",
       "systems/fortyk/templates/actor/actor-corruption.html",
       "systems/fortyk/templates/actor/actor-psykana.html"
