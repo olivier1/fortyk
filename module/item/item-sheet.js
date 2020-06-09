b714e2077948370be65c7a1ad5b5886c1bdaf0cb
olivier1 <mancor161@gmail.com>
2020-06-09 17:57:52 -0400

    fixed a bug
    added propagation when you edit a skill that has children, the children do not keep the skill as their parent anymore, still have to do that for the delete function maybe...

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -4,68 +4,96 @@
  */
 export class FortyKItemSheet extends ItemSheet {
 
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["fortyk", "sheet", "item"],
-      width: 520,
-      height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
-    });
-  }
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["fortyk", "sheet", "item"],
+            width: 520,
+            height: 480,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+        });
+    }
 
-  /** @override */
-  get template() {
-      
-    let type = this.item.type;
-    return `systems/fortyk/templates/item/item-${type}-sheet.html`;
-  
-  
-    // Return a single sheet for all item types.
-    
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.html`.
+    /** @override */
+    get template() {
 
-    // return `${path}/${this.item.data.type}-sheet.html`;
-  }
+        let type = this.item.type;
+        return `systems/fortyk/templates/item/item-${type}-sheet.html`;
 
-  /* -------------------------------------------- */
 
-  /** @override */
-  getData() {
-    const data = super.getData();
-      
-      if(this.item.type==="skill"){
-          //GET THE SKILLS WITH CHILDREN
-          
-          data['skillgroups']=this.actor.items.filter(function(item){return item.data.data.hasChildren.value});
-          
-      }
-    return data;
-  }
+        // Return a single sheet for all item types.
 
-  /* -------------------------------------------- */
+        // Alternatively, you could use the following return statement to do a
+        // unique item sheet by type, like `weapon-sheet.html`.
 
-  /** @override */
-  setPosition(options = {}) {
-    const position = super.setPosition(options);
-    const sheetBody = this.element.find(".sheet-body");
-    const bodyHeight = position.height - 192;
-    sheetBody.css("height", bodyHeight);
-    return position;
-  }
+        // return `${path}/${this.item.data.type}-sheet.html`;
+    }
 
-  /* -------------------------------------------- */
+    /* -------------------------------------------- */
 
-  /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
+    /** @override */
+    getData() {
+        const data = super.getData();
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
+        if(this.item.type==="skill"){
+            //GET THE SKILLS WITH CHILDREN
 
-    // Roll handlers, click handlers, etc. would go here.
-     
-  }
-  
+            data['skillgroups']=this.actor.items.filter(function(item){return item.data.data.hasChildren.value});
+
+        }
+        return data;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    setPosition(options = {}) {
+        const position = super.setPosition(options);
+        const sheetBody = this.element.find(".sheet-body");
+        const bodyHeight = position.height - 192;
+        sheetBody.css("height", bodyHeight);
+        return position;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+        html.find('.skill-type').change(this._onParentChange.bind(this));
+        html.find('.skill-children').click(this._onChildrenClick.bind(this));
+
+        // Roll handlers, click handlers, etc. would go here.
+
+    }
+    //when changing parents check to see if the skill is part of a group if it is change the value of children to false
+    async _onParentChange(event){
+        let value=event.currentTarget.value;
+        console.log(value);
+        if(value!==""){
+            let item=this.item;
+            if(item.data.data.hasChildren){
+                let children=this.actor.items.filter(item=>function(item){return item.data.data.parent.value===this.item.data.data.name.value});
+                for(let i of children){
+                    await i.update({'data.parent.value':""});
+                    
+                }
+                await this.item.update({'data.hasChildren.value':false});
+            }
+
+            
+
+
+        }
+
+    } 
+    async _onChildrenClick(event){
+        let value=event.currentTarget.checked;
+        if(value){
+            await this.item.update({'data.parent.value':""});
+        }
+    }
 }
