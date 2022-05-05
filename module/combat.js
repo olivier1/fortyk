104d23973a311580df839c6b346f335148bcce40
olivier1 <mancor161@gmail.com>
2022-05-05 16:08:28 -0400

    vehicle initiative

--- module/combat.js
+++ module/combat.js
@@ -6,6 +6,11 @@ export const _getInitiativeFormula = function() {
     
     if ( !actor ) return "1d10";
     let init=CONFIG.Combat.initiative.formula;
+    if(actor.type==="vehicle"){
+        let crewRating=actor.data.data.crew.rating;
+        let crewBonus=Math.floor(crewRating/10);
+        init=`1d10 + ${crewBonus} + (${crewRating})/100`;
+    }
     if(actor.getFlag("fortyk","constantvigilance")){
         init= `2d10kh + @characteristics.${actor.getFlag("fortyk","constantvigilance")}.bonus + @secChar.initiative.value + (@characteristics.${actor.getFlag("fortyk","constantvigilance")}.total / 100)`;
     } 
